eeed037d9e9d43a86e165f9879c74fa85c277861
Szymon Radosz <radoszszymon@Szymons-MacBook-Air.local>
2018-12-16 12:05:52 +0100

    babel presets webpack changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require("laravel-mix");
 
 mix
-  .react("resources/assets/js/app.js", "public/js")
+  .js("resources/assets/js/app.js", "public/js")
   .sass("resources/assets/sass/app.scss", "public/css");
